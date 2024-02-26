fb1fe1403dc6bb4216e4cc49c89d2b8d2ce1297c
Manas Khatore <94086185+Manas-Khatore@users.noreply.github.com>
2024-02-26 17:17:06 +0100

    script for creating product table

--- creation.sql
+++ creation.sql
@@ -5,5 +5,8 @@ CREATE TABLE product (
   varietal VARCHAR2(100)
   origin VARCHAR2(100)
   roasting VARCHAR2(100) /* include the different options "natural", "high-roast", "mixture" */
-  dcafprocess 
-  
+  dcafprocess VARCHAR2(100)
+  format VARCHAR2(100)
+  packaging VARCHAR2(100)
+)
+
