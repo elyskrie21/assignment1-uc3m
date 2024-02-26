77385a9c90ffead540c078f37defcff91693a300
Manas Khatore <94086185+Manas-Khatore@users.noreply.github.com>
2024-02-26 17:09:37 +0100

    creating product table

--- creation.sql
+++ creation.sql
@@ -1,1 +1,9 @@
-
+CREATE TABLE product (
+  id VARCHAR2(100)
+  name VARCHAR2(100)
+  coffea VARCHAR2(100)
+  varietal VARCHAR2(100)
+  origin VARCHAR2(100)
+  roasting VARCHAR2(100) /* include the different options "natural", "high-roast", "mixture" */
+  dcafprocess 
+  
