4d903c0e532d62fdb953f91c5f556c7ce9e73b53
Manas Khatore <94086185+Manas-Khatore@users.noreply.github.com>
2024-02-26 17:46:43 +0100

    created reference table

--- creation.sql
+++ creation.sql
@@ -1,13 +1,28 @@
 CREATE TABLE product (
-  id VARCHAR2(100),
-  name VARCHAR2(100),
-  coffea VARCHAR2(100),
-  varietal VARCHAR2(100),
-  origin VARCHAR2(100),
-  roasting VARCHAR2(100) /* include the different options "natural", "high-roast", "mixture" */
-  dcafprocess VARCHAR2(100),
-  format VARCHAR2(100),
-  packaging VARCHAR2(100),
+  id LONG,
+  name CHAR(50),
+  coffea CHAR(20),
+  varietal CHAR(30),
+  origin CHAR(15),
+  roasting CHAR(10) /* include the different options "natural", "high-roast", "mixture" */
+  dcafprocess CHAR(12),
+  format VARCHAR2(20),
+  packaging VARCHAR2(15),
   CONSTRAINT pk_product PRIMARY KEY(id)
 );
 
+CREATE TABLE reference (
+  id LONG,
+  product_id LONG,
+  barcode VARCHAR2(15),
+  retail_price CHAR(14),
+  cur_stock CHAR(5),
+  min_stock CHAR(5) DEFAULT 5,
+  max_stock CHAR(5) DEFAULT 15,
+  CONSTRAINT pk_ref PRIMARY KEY(id),
+  CONSTRAINT valid_cur_stock CHECK (cur_stock >= 0),
+  CONSTRAINT valid_min_stock CHECK (min_stock >= 0),
+  CONSTRAINT valid_max_stock CHECK (max_stock >= 0),
+  CONSTRAINT valid_max_min CHECK (min_stock <= max_stock),
+  CONSTRAINT fk_product FOREIGN KEY(product_id) REFERENCES TO product(id)
+);
